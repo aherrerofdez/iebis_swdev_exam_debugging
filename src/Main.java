a6c2beb19c7e7b4512624783776489ca957982db
Ana <aherrero.mis2016@student.ie.edu>
2019-04-11 12:01:13 +0200

    Third Bug Solved

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,11 @@ public class Main {
 
         switch (random.nextInt(3)) { //bound of random (always counts one less so bound = 3, goes from 0 to 2
             case 0:
-                word = new StringBuffer('Y');
+                word = new StringBuffer("Y"); //stringbuffer constructor allows as input char sequences, strings or ints
             case 1:
-                word = new StringBuffer('F');
+                word = new StringBuffer("F"); //stringbuffer constructor allows as input char sequences, strings or ints
             case 2:
-                word = new StringBuffer('T');
+                word = new StringBuffer("T"); //stringbuffer constructor allows as input char sequences, strings or ints
         }
 
         word.append('o');
